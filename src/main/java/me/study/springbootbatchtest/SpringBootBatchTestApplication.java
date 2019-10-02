f1c13e89e79da6091e6b7c38f33758f995f258a5
lovetodeath <liketodeath@gmail.com>
2019-10-03 00:30:42 +0900

    init

---
+++ src/main/java/me/study/springbootbatchtest/SpringBootBatchTestApplication.java
@@ -0,0 +1,13 @@
+package me.study.springbootbatchtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBatchTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBatchTestApplication.class, args);
+    }
+
+}
